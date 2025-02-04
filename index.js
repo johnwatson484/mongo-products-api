955bad96748b4e363bdf3b1aa1fa75b6540e5e5c
John Watson <john.watson1@defra.gov.uk>
2025-02-04 14:41:44 +0000

    Rename env var

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 
-mongoose.connect(`mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@backenddb.3o5rn.mongodb.net/Node-API?retryWrites=true&w=majority&appName=BackendDB`)
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@backenddb.3o5rn.mongodb.net/Node-API?retryWrites=true&w=majority&appName=BackendDB`)
   .then(() => console.log('Connected!'))
 
 const app = express()
