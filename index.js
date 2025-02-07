7f3e1bffc51cc515a65584e9b4734d59863ae616
John Watson <john.watson1@defra.gov.uk>
2025-02-07 12:50:34 +0000

    Refactor start

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const express = require('express')
 const mongoose = require('mongoose')
 
-mongoose.connect(`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@backenddb.3o5rn.mongodb.net/Node-API?retryWrites=true&w=majority&appName=BackendDB`)
-  .then(() => console.log('Connected!'))
-
 const app = express()
 
 app.get('/', function (req, res) {
   res.send('Hello from Node API')
 })
 
-app.listen(3000)
+mongoose.connect(`mongodb+srv://${ process.env.MONGO_USERNAME }:${ process.env.MONGO_PASSWORD }@backenddb.3o5rn.mongodb.net/Node-API?retryWrites=true&w=majority&appName=BackendDB`)
+  .then(() => {
+    console.log('Connected to MongoDB')
+    app.listen(3000, () => {
+      console.log('Server is running on port 3000')
+    })
+  })
+
+
